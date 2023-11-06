bd1489ac36fd7f0bdcb4eca20187ebf1fe61a472
willefille <willefille@hotmail.com>
2023-11-06 23:03:14 +0100

    First

---
+++ Casino_in_cs/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casino_in_cs {
+    class Player {
+
+        public Player(int _money, string _name) {
+            Money = _money;
+            Name = _name;
+        }
+
+        private int money;
+        public int Money {
+            get { return money; }
+            set { money = value; } //Make check later if the money is less than 0, eg. game over
+        }
+        private string name;
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+    }
+}
---
+++ Casino_in_cs/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Casino_in_cs {
+    class Program {
+        static void Main(string[] args) {
+            Menu menu = new Menu();
+
+            menu.WriteWelcomeText();
+        }
+    }
+}
